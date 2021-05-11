866d30b8fdbc3ed8d299a364f0859280b4b428a0
tejasmorkar <tejasmorkar@gmail.com>
2021-05-12 01:08:19 +0530

    change call for action

--- src/components/helpdeskcard/help-desk-card.component.jsx
+++ src/components/helpdeskcard/help-desk-card.component.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 
 function HelpDeskCard(props) {
-  return (
-    <div>
-      <Card style={{ margin: "20px 10px 10px 10px" }}>
-        <Card.Img style={{ height: "170px" }} variant="top" src={props.img} />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{props.desc}</Card.Text>
-          <Button style={{backgroundColor:"#018361"}}>Visit Commitee</Button>
-        </Card.Body>
-      </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card style={{ margin: "20px 10px 10px 10px" }}>
+                <Card.Img
+                    style={{ height: "170px" }}
+                    variant="top"
+                    src={props.img}
+                />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>{props.desc}</Card.Text>
+                    <Button style={{ backgroundColor: "#018361" }}>
+                        Ask for help
+                    </Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
 
 export default HelpDeskCard;
