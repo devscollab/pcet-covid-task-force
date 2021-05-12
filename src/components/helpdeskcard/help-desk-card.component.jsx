88168dd0639f52f549552fc9eb3c87cdc0d3deda
tejasmorkar <tejasmorkar@gmail.com>
2021-05-12 15:20:15 +0530

    change header tip format and helpdesk cards layout

--- src/components/helpdeskcard/help-desk-card.component.jsx
+++ src/components/helpdeskcard/help-desk-card.component.jsx
@@ -3,22 +3,25 @@ import { Button, Card } from "react-bootstrap";
 
 function HelpDeskCard(props) {
     return (
-        <div>
-            <Card style={{ margin: "20px 10px 10px 10px" }}>
-                <Card.Img
-                    style={{ height: "170px" }}
-                    variant="top"
-                    src={props.img}
-                />
-                <Card.Body>
-                    <Card.Title>{props.title}</Card.Title>
-                    <Card.Text>{props.desc}</Card.Text>
-                    <Button style={{ backgroundColor: "#018361" }}>
-                        {props.button}
-                    </Button>
-                </Card.Body>
-            </Card>
-        </div>
+        <Card
+            style={{
+                margin: "20px 10px 10px 10px",
+                height: "100%",
+            }}
+        >
+            <Card.Img
+                style={{ height: "170px" }}
+                variant="top"
+                src={props.img}
+            />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.desc}</Card.Text>
+                <Button style={{ backgroundColor: "#018361" }}>
+                    {props.button}
+                </Button>
+            </Card.Body>
+        </Card>
     );
 }
 
