4b2e29e78ff2d93daeeaf787e450f4800a9825a6
unknown <shubhamdudhal9@gmail.com>
2021-05-12 18:32:48 +0530

    volunteer

--- src/components/helpdeskcard/help-desk-card.component.jsx
+++ src/components/helpdeskcard/help-desk-card.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
-
 function HelpDeskCard(props) {
     return (
         <Card
---
+++ src/components/volunteer-cards/volunteer-cards.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+function VolunteerCards(props) {
+    return (
+        <Card
+            style={{
+
+                margin: "20px 10px 10px 10px",
+                height: "100%",
+            }}
+        >
+            <Card.Img
+                style={{ height: "170px" }}
+                variant="top"
+                src={props.img}
+            />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.desc}</Card.Text>
+                <Button style={{ backgroundColor: "#018361" }}>
+                    {props.button}
+                </Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default VolunteerCards;
