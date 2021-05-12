923878a6ff0fcb16777474b363ea2341ae8fccea
Yash0411 <ynm0411@gmail.com>
2021-05-12 17:39:17 +0530

    add What We Do

--- src/pages/what-we-do/data.js
+++ src/pages/what-we-do/data.js
@@ -22,53 +22,5 @@ export const whatWeDoData = [
         desc: "Critical cases might need Oxygen beds and ventilator arrangements and we can help you get correct information and contact details for the same",
         img: "https://drive.google.com/uc?id=1euis01Dp391o5SbnYrRUZ8XxO1QU_CKO",
         button: "Ask for help",
-    },
-    {
-        title: "Medicines",
-        desc: "We will help you find verified resources for required medecines and also help get them delivered to you if you can't go out due to quarantine or such reasons",
-        img: "https://drive.google.com/uc?id=1FMhXpEwFl78FItgkiL64bUvxoo-WVb6E",
-        button: "Ask for help",
-    },
-    {
-        title: "Blood and Plasma",
-        desc: "If you need Blood/ Plasma we will help find contacts and people willing to donate and connect them with you",
-        img: "https://drive.google.com/uc?id=1syursfT_lLyug-Tutjyq4MNy0s3KgHDe",
-        button: "Ask for help",
-    },
-    {
-        title: "Moral Support",
-        desc: "Hello world",
-        img: "https://drive.google.com/uc?id=1vj_HlFBZUcrUxpbLslPXwdVT7rbNRdbh",
-        button: "Ask for help",
-    },
-    {
-        title: "Vaccination",
-        desc: "Hello world",
-        img: "https://drive.google.com/uc?id=1RcLL41CIg6SrZqSc1kw_DnhwBlfPptKz",
-        button: "Ask for help",
-    },
-    {
-        title: "Doctors for Suggestions",
-        desc: "Hello world",
-        img: "https://drive.google.com/uc?id=1-NA4iWKXhSLNzYQrKdm_nJawDu78Lz4a",
-        button: "Ask for help",
-    },
-    {
-        title: "Food Availability for the needy",
-        desc: "We will help you find verified resources for food necessities and also help get the food delivered to you if you can't go out due to quarantine or such reasons",
-        img: "https://drive.google.com/uc?id=1ut4ZWctD_tySlPsnoNLmmj7A5vmgO2X5",
-        button: "Ask for help",
-    },
-    {
-        title: "Post Covid Communication",
-        desc: "Hello world",
-        img: "https://drive.google.com/uc?id=1ERAzJulbR2EuWI12FQ116PvQtHQ0FW8O",
-        button: "Ask for help",
-    },
-    {
-        title: "Technical Support",
-        desc: "Hello world",
-        img: "https://drive.google.com/uc?id=188iyL7dKTyvHqvj9ahNnz9-Tfu-cTtfr",
-        button: "Ask for help",
-    },
+    }
 ];
