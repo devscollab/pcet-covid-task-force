c5a30aa0b05b9dc9ca284a121cd97d5929e77351
unknown <shubhamdudhal9@gmail.com>
2021-05-12 20:03:48 +0530

    Volunteer Cards Added

--- src/components/volunteer-cards/volunteer-cards.component.jsx
+++ src/components/volunteer-cards/volunteer-cards.component.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import './volunteer-cards.style.scss'
 function VolunteerCards(props) {
     return (
-        <Card
+        <Card className="volcard"
             style={{
-
-                margin: "20px 10px 10px 10px",
-                height: "100%",
+               minwidth:"550px"    
             }}
         >
             <Card.Img
-                style={{ height: "170px" }}
+                style={{ height: "275px" }}
                 variant="top"
                 src={props.img}
             />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>{props.desc}</Card.Text>
-                <Button style={{ backgroundColor: "#018361" }}>
+                <Button className="helpercard-button" style={{ backgroundColor: "#018361" }}>
                     {props.button}
                 </Button>
             </Card.Body>
